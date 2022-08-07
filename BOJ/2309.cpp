7587b205bbfa67d80f109dce384ef03a7503c564
lkt9899 <80976609+lkt9899@users.noreply.github.com>
2022-08-08 02:12:51 +0900

    [BOJ] 2309 : 일곱 난쟁이

--- BOJ/2309.cpp
+++ BOJ/2309.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-constexpr int MAX_INPUT = 9;
+#define MAX_INPUT 9
 
 using namespace std;
 
 int arr[MAX_INPUT];
 
-void calc() {
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int total = 0;
-    for(int i = 0; i < 9; i++) {
+
+    for (int i = 0; i < MAX_INPUT; i++)
+    {
         cin >> arr[i];
         total += arr[i];
     }
-    
-    for(int i = 0; i < 8; i++) {
-        for(int j = i+1; j < 9; j++) {
-            if(total - arr[i] - arr[j] == 100) {
-                arr[i] = 0; arr[j] = 0;
-                sort(arr, arr+9);
+
+    for (int i = 0; i < MAX_INPUT - 1; i++)
+    {
+        for (int j = i + 1; j < MAX_INPUT; j++)
+        {
+            if (total - arr[i] - arr[j] == 100)
+            {
+                arr[i] = -1;
+                arr[j] = -1;
                 break;
             }
         }
-        if(arr[i] == 0) break;
+        if (arr[i] == -1)
+            break;
     }
 
-    for(int i = 2; i < 9; i++) {
+    sort(arr, arr + MAX_INPUT);
+
+    for (int i = 2; i < MAX_INPUT; i++)
+    {
         cout << arr[i] << "\n";
     }
-}
-
-int main()
-{
-    fio;
-    calc();
 
     return 0;
 }
