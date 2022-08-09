c2aeaa85a140d2c446756906f54acac1ebc8ae0c
lkt9899 <80976609+lkt9899@users.noreply.github.com>
2022-08-09 23:20:03 +0900

    [BOJ] 1007 : A / B

---
+++ BOJ/1007.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fio;
+    double a, b;
+    cin >> a >> b;
+    printf("%.10f", a / b);
+    return 0;
+}
