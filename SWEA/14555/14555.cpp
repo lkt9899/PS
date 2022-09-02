9089e2d3845f730a18dbd5e06d4127b96b43e6a3
lkt9899 <lkt9899@naver.com>
2022-09-02 14:52:30 +0900

    [SWEA] 14555 : 공과 잡초

---
+++ SWEA/14555/14555.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+string s;
+int res;
+
+int main(int argc, char** argv)
+{
+    fio;
+    int test_case;
+    int T;
+
+    freopen("14555.txt", "r", stdin);
+    cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        res = 0;
+        cin >> s;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '|' && (s[i-1] == '(' || s[i+1] == ')')) res++;
+            else if(s[i] == '(' && s[i+1] == ')') {
+                i++; res++;
+            }
+        }
+        cout << "#" << test_case << " " << res << "\n";
+    }
+
+    return 0;
+}
