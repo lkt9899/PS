e10aa67c60b02ca5b2073ec382f5077cf0a8bad4
이경태 <80976609+lkt9899@users.noreply.github.com>
2022-09-03 21:42:11 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+using namespace std;
+
+int main()
+{
+    fio;
+    cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n";
+    return 0;
+}
