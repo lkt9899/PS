c7bc8fee97e2094fd1b06260c1c5dc156bc6df63
이경태 <80976609+lkt9899@users.noreply.github.com>
2022-09-03 20:56:06 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+using namespace std;
+
+int main()
+{
+    fio;
+    int A, B; cin >> A >> B;
+    cout << A + B;
+
+    return 0;
+}
