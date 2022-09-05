90a92e5f8f8a2c08d7f1ee6c93745c4425d14410
이경태 <80976609+lkt9899@users.noreply.github.com>
2022-09-05 10:31:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+
+    cin >> N;
+    for(int i = 1; i <= N; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
