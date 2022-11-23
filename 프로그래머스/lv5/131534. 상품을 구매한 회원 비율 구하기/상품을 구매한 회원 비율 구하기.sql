4d615da17b8594128015b0b4ba4539fcaafa3b69
이경태 <80976609+lkt9899@users.noreply.github.com>
2022-11-23 17:48:44 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,28 +1,55 @@
 -- 코드를 입력하세요
-SET @IC = (
-    SELECT
-        COUNT(USER_ID)
-    FROM
-        USER_INFO
-    WHERE
-        JOINED LIKE "2021%");
+# SET @IC = (
+#     SELECT
+#         COUNT(USER_ID)
+#     FROM
+#         USER_INFO
+#     WHERE
+#         JOINED LIKE "2021%");
 
-SELECT 
-    YEAR,
-    MONTH,
-    COUNT(*) PURCHASED_USERS,
-    ROUND(COUNT(*) / @IC, 1) PURCHASED_RATIO
-FROM (
-    SELECT DISTINCT
-        YEAR(S.SALES_DATE) AS YEAR,
-        MONTH(S.SALES_DATE) AS MONTH,
-        U.USER_ID
-    FROM
-        ONLINE_SALE S
-    JOIN
-        USER_INFO U
-    ON
-        S.USER_ID = U.USER_ID AND
-        YEAR(JOINED) = 2021) J
-GROUP BY YEAR, MONTH
-ORDER BY YEAR, MONTH;
+# SELECT 
+#     YEAR,
+#     MONTH,
+#     COUNT(*) PURCHASED_USERS,
+#     ROUND(COUNT(*) / @IC, 1) PURCHASED_RATIO
+# FROM (
+#     SELECT DISTINCT
+#         YEAR(S.SALES_DATE) AS YEAR,
+#         MONTH(S.SALES_DATE) AS MONTH,
+#         U.USER_ID
+#     FROM
+#         ONLINE_SALE S
+#     INNER JOIN
+#         USER_INFO U
+#     ON
+#         S.USER_ID = U.USER_ID AND
+#         YEAR(JOINED) = 2021) J
+# GROUP BY YEAR, MONTH
+# ORDER BY YEAR, MONTH;
+
+SELECT
+    YEAR(S.SALES_DATE) YEAR,
+    MONTH(S.SALES_DATE) MONTH,
+    COUNT(DISTINCT S.USER_iD) PURCHASED_USERS,
+    ROUND(
+        COUNT(DISTINCT S.USER_ID) / (
+            SELECT 
+                COUNT(*)
+            FROM
+                USER_INFO
+            WHERE
+                JOINED LIKE "2021%"), 1) PURCHASED_RATIO
+FROM
+    ONLINE_SALE S
+    JOIN (
+        SELECT
+            USER_ID
+        FROM
+            USER_INFO
+        WHERE
+            JOINED LIKE "2021%") U
+    ON S.USER_ID = U.USER_ID
+GROUP BY
+    YEAR, MONTH
+ORDER BY 
+    YEAR, MONTH;
