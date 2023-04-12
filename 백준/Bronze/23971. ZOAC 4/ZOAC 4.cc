0af0cf4e755ac3be177d5e3c64e7d33c8ac99ef4
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-04-12 21:59:48 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -6,6 +6,6 @@ double h, w, n, m;
 
 int main() {
     cin >> h >> w >> n >> m;
-    cout << (long long)ceil(h/++n) * (long long)ceil(w/++m);
+    cout << (int)(ceil(h/++n) * ceil(w/++m));
     return 0;
 }
