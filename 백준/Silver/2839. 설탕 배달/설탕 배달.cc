ed6433407c3f804f363f0870c51f75becdd8e163
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-07-31 03:11:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,33 +1,16 @@
 #include <iostream>
-#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
 using namespace std;
 
-int N, ans;
-
-void input() { cin >> N; }
-
-void sol() {
-    ans = 0;
-    while (N >= 0) {
-        if (N % 5 == 0) {
-            ans += (N / 5);
-            cout << ans << "\n";
-            return;
-        }
-        N -= 3;
-        ans++;
-    }
-    cout << -1 << "\n";
-}
-
-void run() {
-    input();
-    sol();
-}
-
 int main() {
-    fio;
-    run();
+    int N, ans;
+    cin >> N;
+    ans = N / 5;
+    
+    if (N == 4 || N == 7) cout << -1;
+    else if (N % 5 == 0) cout << ans;
+    else if (N % 5 == 1 || N % 5 == 3) cout << ans + 1;
+    else if (N % 5 == 2 || N % 5 == 4) cout << ans + 2;
+    
     return 0;
 }
