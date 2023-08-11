23ec660194ea7667640c2c10e27d08c21ee03edd
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-08-11 15:40:36 +0900

    [Silver IV] Title: 설탕 배달, Time: 72 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,16 +1,23 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine());
-        int ans = N / 5;
-
-        if (N == 4 || N == 7) ans = -1;
-        else if (N % 5 == 1 || N % 5 == 3) ans += 1;
-        else if (N % 5 == 2 || N % 5 == 4)
-            ans += 2;
-        System.out.println(ans); 
+        
+        int[] dp = new int[5001];
+        Arrays.fill(dp, -1);
+        dp[3] = dp[5] = 1;
+        
+        for (int i = 6; i <= N; i++) {
+            if (dp[i - 3] != -1)
+                dp[i] = dp[i - 3] + 1;
+            if (dp[i - 5] != -1)
+                dp[i] = dp[i] != -1 ? Math.min(dp[i], dp[i - 5] + 1) : dp[i - 5] + 1;
+        }
+        System.out.println(dp[N]);
+        br.close();
     }
 }
