1faa3d5c32a5ad79ba40b5bdd2b14e3232541192
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-08-12 13:37:49 +0900

    [D3] Title: 이진수 표현, Time: 10 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,21 +1,40 @@
-#include<iostream>
-using namespace std;
- 
-int main(int argc, char** argv)
-{
-    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    int test_case;
-    int T;  cin>>T;
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        int N, M;   cin >> N >> M;
-        int chk = (1 << N) - 1;
-        int tmp = chk;
- 
-        chk &= M;
-        cout << "#" << test_case << " ";
-        if(chk == tmp) cout << "ON\n";
-        else cout << "OFF\n";
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+
+    public static void main(String[] args) throws Exception {
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#" + test_case + " ");
+            run();
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
-    return 0;
+
+    static void run() throws Exception {
+        input();
+        for (int i = 0; i < N; i++) {
+            if ((M & (1 << i)) == 0) {
+                sb.append("OFF\n");
+                return;
+            }
+        }
+        sb.append("ON\n");
+    }
+
+    static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+    }
 }
