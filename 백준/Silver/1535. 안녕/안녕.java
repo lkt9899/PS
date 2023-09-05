90226bbf6c2b2c50fefa19903cc7ecb541d7178a
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-09-05 20:43:42 +0900

    [Silver II] Title: 안녕, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -1,17 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    
+
     static int N, ans;
     static int[] lose, happy;
     static int[] dp = new int[101];
 
-
     public static void main(String[] args) throws Exception {
+        input();
+        run();
+    }
+
+    static void input() throws Exception {
         N = Integer.parseInt(br.readLine());
         lose = new int[N];
         happy = new int[N];
@@ -25,12 +28,12 @@ public class Main {
         for (int i = 0; i < N; i++) {
             happy[i] = Integer.parseInt(st.nextToken());
         }
+    }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 100; j > lose[i]; j--) {
+    static void run() throws Exception {
+        for (int i = 0; i < N; i++)
+            for (int j = 100; j > lose[i]; j--)
                 dp[j] = Math.max(dp[j - lose[i]] + happy[i], dp[j]);
-            }
-        }
 
         System.out.println(dp[100]);
     }
