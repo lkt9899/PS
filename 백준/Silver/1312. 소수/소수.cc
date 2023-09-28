3717c25921d6e74f5c0aab718f1fe677063e5fb2
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-09-29 00:13:02 +0900

    [Silver V] Title: 소수, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, N, ans;
+    cin >> A >> B >> N;
+    A %= B;
+    while(N--) {
+        A = (A % B) * 10;
+        ans = A / B;
+        A -= (B * ans);
+    }
+    cout << ans;
+}
