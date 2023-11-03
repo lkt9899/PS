066a0db3c8032124697631bc24c9bad1618089c4
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-11-03 09:56:45 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,30 +1,23 @@
 #include <iostream>
 #define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-
+#define MAX 100
 using namespace std;
 
-const int MAX = 100;
 int N;
 int adj[MAX][MAX];
 
 void input() {
     cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
             cin >> adj[i][j];
-        }
-    }
 }
 
 void FW() {
-    for (int k = 0; k < N; k++) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (adj[i][k] && adj[k][j]) adj[i][j] = 1;
-            }
-        }
-    }
+    for (int k = 0; k < N; k++)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                adj[i][j] |= (adj[i][k] & adj[k][j]);
 }
 
 void print() {
@@ -36,14 +29,10 @@ void print() {
     }
 }
 
-void sol() {
-    FW();
-    print();
-}
-
 void run() {
     input();
-    sol();
+    FW();
+    print();
 }
 
 int main() {
