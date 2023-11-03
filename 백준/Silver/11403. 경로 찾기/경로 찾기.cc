aa8e475de4e15a7d1ef9e0c3e8025f022d6a5046
이경태 <80976609+lkt9899@users.noreply.github.com>
2023-11-03 10:00:06 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -3,40 +3,25 @@
 #define MAX 100
 using namespace std;
 
-int N;
-int adj[MAX][MAX];
-
-void input() {
+int main() {
+    fio;
+    int N;
     cin >> N;
+    int adj[N][N];
+    
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             cin >> adj[i][j];
-}
-
-void FW() {
+    
     for (int k = 0; k < N; k++)
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 adj[i][j] |= (adj[i][k] & adj[k][j]);
-}
-
-void print() {
+    
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; j++)
             cout << adj[i][j] << " ";
-        }
         cout << "\n";
     }
-}
-
-void run() {
-    input();
-    FW();
-    print();
-}
-
-int main() {
-    fio;
-    run();
     return 0;
 }
