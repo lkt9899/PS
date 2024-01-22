5104e27978a0323e7b686641645a043d8c6c6275
이경태 <80976609+lkt9899@users.noreply.github.com>
2024-01-22 23:46:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,30 +1,21 @@
 #include <iostream>
 #define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-
 #define i2(a, b) cin >> a >> b
 #define fi0(e) for (int i = 0; i < e; i++)
 
-#define MAX_K 100001
-
 using namespace std;
-
-int N, K;
-int dp[MAX_K];
-
-void sol() {
-    i2(N, K);
-    fi0(N) {
-        int w, v;
-        i2(w, v);
-        for (int j = K; j >= w; j--) {
-            dp[j] = max(dp[j], dp[j - w] + v);
-        }
-    }
-    cout << dp[K];
-}
 
 int main() {
     fio;
-    sol();
+    int N, K, W, V;
+    int dp[100001] = {0, };
+    i2(N, K);
+    fi0(N) {
+        i2(W, V);
+        for (int j = K; j >= W; j--) {
+            dp[j] = dp[j] > dp[j - W] + V ? dp[j] : dp[j - W] + V;
+        }
+    }
+    cout << dp[K];
     return 0;
 }
