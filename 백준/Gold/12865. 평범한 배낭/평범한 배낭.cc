571a28a09bb423a06c5c53d8d05b71764d703bc4
이경태 <80976609+lkt9899@users.noreply.github.com>
2024-01-22 23:58:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,13 +1,12 @@
 #include <ios>
 
 int main() {
-    int N, K, W, V;
-    int dp[K + scanf("%d %d", &N, &K)] = { };
-    while(N--) {
+    int N, K, W, V, j;
+    scanf("%d %d", &N, &K);
+    int dp[K + 1] = {};
+    while (N--) {
         scanf("%d %d", &W, &V);
-        for (int j = K; j >= W; j--)
-            dp[j] = std::max(dp[j], dp[j - W] + V);
+        for (j = K - W; j >= 0; j--) dp[j + W] = dp[j + W] > dp[j] + V ? dp[j + W] : dp[j] + V;
     }
     printf("%d", dp[K]);
-    return 0;
 }
